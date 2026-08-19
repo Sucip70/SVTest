f2e531f87540d3feb595b3e5a8ce177573e26969
Sucip70 <ahmad0sucipto@gmail.com>
2026-08-19 18:55:41 +0700

    Initiate project be

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/Sucip70/SVTest-demo-be
+
+go 1.26.4
+
+require (
+	filippo.io/edwards25519 v1.2.0 // indirect
+	github.com/go-sql-driver/mysql v1.10.0 // indirect
+	github.com/golang-migrate/migrate/v4 v4.19.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
---
+++ migrations/000001_create_posts_table.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `posts` (
+  `Id` int unsigned NOT NULL AUTO_INCREMENT,
+  `Title` varchar(200) DEFAULT NULL,
+  `Content` text,
+  `Category` varchar(100) DEFAULT NULL,
+  `Created_date` timestamp NULL DEFAULT NULL,
+  `Updated_date` timestamp NULL DEFAULT NULL,
+  `Status` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
